000a0d8d769340824a7d871e2797f88ae4075ae7
Rana Danish <itzdanishpb65@gmail.com>
2025-06-19 10:34:02 +0500

    Footor Updated

--- src/Components/Footor.jsx
+++ src/Components/Footor.jsx
@@ -3,18 +3,31 @@ import logotext from "../assets/logotext.png";
 
 const Footor = () => {
   return (
-    <footer className="bg-gray-900 text-white py-10 px-6">
-      <div className="max-w-7xl flex flex-wrap items-center   justify-between gap-10">
+    <footer className="bg-gray-900 text-white py-5 px-6">
+      <div className="max-w-7xl flex flex-wrap items-center   justify-evenly gap-10">
         <div>
-          <h2 className="text-xl font-semibold mb-3">Contact Us</h2>
+          <h2 className="text-2xl font-semibold mb-3">Our Locations</h2>
           <p className="text-sm leading-relaxed">
+            <h1 className="text-xl font-semibold my-3">Pakistan's Address:</h1>
             #130 Plaza 289, First Floor,
             <br />
             Q Block, Commercial Lane,
             <br />
             Phase 2, DHA, Lahore. <br />
-            2410 San Ramon Valley Blvd #130 San Ramon, CA 94583 United States
+            <h1 className="text-xl font-semibold my-3">USA Address:</h1>
+            2410 San Ramon Valley Blvd #130 San Ramon <br /> CA 94583 United States
           </p>
+        </div>
+        <img
+          src={logotext}
+          alt="Company Logo"
+          className=" w-50 object-contain hidden md:block "
+        />
+
+        {/* Navigation */}
+        <div className="flex flex-col ">
+          <h2 className="text-2xl font-semibold">Contact Links</h2>
+          <h3>Email:info@greenknight.io</h3>
           <p className="mt-3 text-sm">
             WhatsApp:{" "}
             <a
@@ -27,42 +40,34 @@ const Footor = () => {
             </a>
           </p>
         </div>
-
-        {/* Navigation */}
-        <div className="flex flex-col gap-2 text-center">
-          <h2 className="text-3xl font-semibold mb-3">Quick Links</h2>
-          <a
-            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-            className="hover:text-gray-300 cursor-pointer"
-          >
-            Home
-          </a>
-          <a href="#about" className="hover:text-gray-300">
-            About
-          </a>
-          <a href="#services" className="hover:text-gray-300">
-            Services
-          </a>
-          <a href="#clients" className="hover:text-gray-300">
-            Clients
-          </a>
-          <a href="#team" className="hover:text-gray-300">
-            Team
-          </a>
-          <a href="#contact" className="hover:text-gray-300">
-            Contact
-          </a>
-        </div>
-
-        <img
-          src={logotext}
-          alt="Company Logo"
-          className=" w-70 object-contain hidden md:block "
-        />
       </div>
 
       {/* Bottom Copyright */}
       <div className="text-center text-sm text-gray-400 mt-8">
+        <div className="flex justify-evenly flex-wrap mb-4 gap-2 text-center">
+        
+          <a
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            className="hover:text-gray-300 text-xl underline cursor-pointer"
+          >
+            Home
+          </a>
+          <a href="#about" className="hover:text-gray-300 underline text-xl">
+            About
+          </a>
+          <a href="#services" className="hover:text-gray-300 underline text-xl">
+            Services
+          </a>
+          <a href="#clients" className="hover:text-gray-300 underline text-xl">
+            Clients
+          </a>
+          <a href="#team" className="hover:text-gray-300 underline text-xl">
+            Team
+          </a>
+          <a href="#contact" className="hover:text-gray-300 underline text-xl">
+            Contact
+          </a>
+        </div>
         &copy; {new Date().getFullYear()} Green Knight. All rights reserved.
       </div>
     </footer>
